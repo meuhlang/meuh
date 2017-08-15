a3006730bdac4a2160e316a40097d5d39a6fb3af
Laurent Verdoïa <verdoialaurent@gmail.com>
2017-08-15 10:36:08 +0200

    Start to write syntax related documentation

---
+++ docs/syntax.js
@@ -0,0 +1,9 @@
+// vim:set ts=4 sw=4 tw=100 et:
+
+window.find = document.querySelector.bind(document);
+
+Diagram(
+    NonTerminal('package-declaration')
+    ,NonTerminal('import-declaration')
+    ,NonTerminal('class-declaration')
+).addTo(find('.compilation-unit'));
