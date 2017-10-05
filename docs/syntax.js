f6edb92b3a5018496619a650915f098216fabfd1
Laurent Verdoïa <verdoialaurent@gmail.com>
2017-10-05 06:59:59 +0200

    Improve syntax

--- docs/syntax.js
+++ docs/syntax.js
@@ -4,7 +4,9 @@ window.find = document.querySelector.bind(document);
 
 Diagram(
     NonTerminal('package-declaration', '#package-declaration')
-    ,NonTerminal('import-declaration', '#import-declaration')
+    ,Optional(
+        NonTerminal('import-declaration', '#import-declaration')
+    )
     ,NonTerminal('class-declaration', '#class-declaration')
 ).addTo(find('.compilation-unit'));
 
@@ -13,3 +15,57 @@ Diagram(
     ,NonTerminal('package-name', '#package-name')
     ,Terminal(';')
 ).addTo(find('.package-declaration'));
+
+Diagram(
+    NonTerminal('identifier', '#identifier')
+    ,ZeroOrMore(Sequence(
+        Terminal('.')
+        ,NonTerminal('identifier', '#identifier')
+    ))
+).addTo(find('.package-name'));
+
+Diagram(
+    Terminal('import')
+    ,Choice(0,
+        Sequence(
+            NonTerminal('package-name', '#package-name')
+            ,Terminal('.')
+            ,NonTerminal('identifier', '#identifier')
+            ,Optional(Sequence(
+                Terminal('as')
+                ,NonTerminal('identifier', '#identifier')
+            ))
+        )
+        ,Sequence(
+            Terminal('static')
+            ,NonTerminal('package-name', '#package-name')
+            ,Terminal('.')
+            ,NonTerminal('identifier', '#identifier')
+            ,Terminal('.')
+            ,Choice(
+                0
+                ,Terminal('*')
+                ,Sequence(
+                    NonTerminal('identifier', '#identifier')
+                    ,Optional(Sequence(
+                        Terminal('as')
+                        ,NonTerminal('identifier', '#identifier')
+                    ))
+                )
+            )
+        )
+    )
+    ,Terminal(';')
+).addTo(find('.import-declaration'));
+
+Diagram(
+    Terminal('class')
+    ,NonTerminal('identifier', '#identifier')
+    ,Terminal('{')
+    ,Optional(Choice(0
+        ,NonTerminal('member-declaration', '#member-variable-declaration')
+        ,NonTerminal('method-declaration', '#method-declaration')
+    ))
+    ,Terminal('}')
+    ,Terminal(';')
+).addTo(find('.class-declaration'));
