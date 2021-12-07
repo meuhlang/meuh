667976c09ff7dd9b573b3c4c594860506e8397eb
Laurent Verdoïa <verdoialaurent@gmail.com>
2021-12-07 22:45:43 +0000

    Deploying to gh-pages from @ meuhlang/meuh@b3cb0dc721cc29194186ef5db254180cf25f3f8e 🚀

--- maven/core/testapidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"io.github.meuhlang.meuh.core","c":"DummyDataTest","l":"DummyDataTest()","url":"%3Cinit%3E()"},{"p":"io.github.meuhlang.meuh.core","c":"DummyDataTest","l":"testConstructor()"}]
--- maven/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.meuhlang.meuh.cli","l":"DummyCommandTest"},{"p":"io.github.meuhlang.meuh.core","l":"DummyDataTest"}]
